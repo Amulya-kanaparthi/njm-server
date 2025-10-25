a1df1fbf34c9bad21f3462eb6188f830ef836d91
Amulya-kanaparthi <kanaparthiamulya16@gmail.com>
2025-10-25 10:34:28 +0530

    .

--- middleware/connectDB.js
+++ middleware/connectDB.js
@@ -1,25 +1,6 @@
 // middleware/connectDB.js
 import mongoose from 'mongoose';
-// import mysql from 'mysql2';
 
-// const mongoURL = process.env.MONGO_URL;
-// /// MySQL Connection
-// const db = mysql.createConnection({
-//     host: process.env.MYSQLHOST,
-//     user: process.env.MYSQLUSER,
-//     password: process.env.MYSQLPASSWORD,
-//     database: process.env.MYSQLDATABASE,
-// });
-
-// db.connect((err) => {
-//     if (err) {
-//         console.error('Error connecting to database :', err);
-//         return;
-//     }
-//     console.log('Connected to database');
-// });
-
-// export default db;
 const mongoUri = process.env.MONGO_URI;
 /// MongoDB Connection
 export const connectMongoDB = async () => {
